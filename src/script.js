15c8406e24de38f9f1fa8bede2b40bb2e5249094
t0uu <srtouzet@hotmail.com>
2022-09-11 11:49:32 -0300

    add chapter

---
+++ src/script.js
@@ -0,0 +1,155 @@
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import imageMaterial from './textures/Warning_Sign_Poison_001_basecolor.png'
+
+/**
+ * Textures
+ */
+// Method large
+// const image = new Image()
+// const texture = new THREE.Texture(image)
+// image.onload = () => {
+//     texture.needsUpdate = true
+// }
+
+// image.src = './textures/door/color.jpg'
+
+
+const loadingManager = new THREE.LoadingManager()
+
+// loadingManager.onStart = () => {
+// console.log('loading starter');
+// }
+// loadingManager.onLoad = () => {
+//     console.log('onLoaded');
+// }
+// loadingManager.onProgress = () => {
+//     console.log('loading progressing');
+// }
+// loadingManager.onError = () => {
+//     console.log('onError');
+// }
+// Method Short
+const textureLoader = new THREE.TextureLoader(loadingManager)
+const baseTexture = textureLoader.load(imageMaterial,
+// () => {
+//     console.log('load');
+// },
+// () => {
+//     console.log('progress');
+// },
+// () => {
+//     console.log('error');
+// }
+)
+const alphaTexture = textureLoader.load('./textures/door/alpha.jpg')
+const heightTexture = textureLoader.load('./textures/door/height.jpg')
+const normalTexture = textureLoader.load('./textures/door/normal.jpg')
+const ambientOcclusionTexture = textureLoader.load('./textures/door/ambientOcclusion.jpg')
+const mentalnessTexture = textureLoader.load('./textures/door/metalness.jpg')
+const roughnessTexture = textureLoader.load('./textures/door/roughness.jpg')
+
+// Repeat Texture
+// colorTexture.repeat.x = 2
+// colorTexture.repeat.y = 3
+// colorTexture.wrapS = THREE.MirroredRepeatWrapping
+// colorTexture.wrapT = THREE.RepeatWrapping
+
+// colorTexture.offset.x = 0.5
+// colorTexture.offset.y = 0.5
+
+// colorTexture.rotation = Math.PI * 0.25
+// colorTexture.center.x = 0.5
+// colorTexture.center.y = 0.5
+baseTexture.generateMipmaps = false
+baseTexture.minFilter = THREE.NearestFilter
+baseTexture.magFilter = THREE.NearestFilter
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Object
+ */
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+// const geometry = new THREE.SphereGeometry(1,32,32)
+// const geometry = new THREE.ConeGeometry(1,1,32)
+// const geometry = new THREE.TorusGeometry(1,0.30,32,100)
+
+console.log(geometry.attributes.uv);
+const material = new THREE.MeshBasicMaterial({ map: baseTexture })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 1
+camera.position.y = 1
+camera.position.z = 1
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
